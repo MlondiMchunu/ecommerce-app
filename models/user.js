5713c46dcb7d6c743366b2cccbd1895bfd8daac8
Stopa <sikhuliso@gmail.com>
2024-05-20 20:56:12 +0200

    virtual fields & methods

--- models/user.js
+++ models/user.js
@@ -32,4 +32,32 @@ const userSchema = new mongoose.Schema({
         type: Array,
         default: []
     }
-},{timestamps:true})
+},
+    { timestamps: true }
+);
+
+//virtual field 
+userSchema.virtual('password')
+.set(function(password){
+    this._password = password;
+    this.salt = uuidv1();
+    this.hashed_password = this.encryptPassword(password)
+})
+.get(function(){
+    return this._password
+})
+
+userSchema.methods = {
+    encryptPassword: function(password){
+        if(!password) return"";
+        try{
+            return crypto.createHmac("sha1",this.salt)
+                            .update(password)
+                            .digest("hex")
+        } catch(err){
+            return"";
+        }
+    }
+};
+
+module.exports = mongose.model("User",userSchema);
