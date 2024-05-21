613f28b92b5004fbd269092db755bed9d3881481
Stopa <sikhuliso@gmail.com>
2024-05-21 19:27:32 +0200

    express-validator

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const {errorHandler} = require('../helpers/dbErrorHandler')
 
 
 exports.signup = async(req,res)=>{
@@ -7,10 +8,17 @@ exports.signup = async(req,res)=>{
     const user = new User(req.body);
 
     try{
+        //user.salt = undefined;
+        //user.hashed_password = undefined;
+
         const savedUser = await user.save();
+
+        savedUser.salt = undefined;
+        savedUser.hashed_password = undefined;
+        
         res.status(201).json({message:"User created succesfully",user:savedUser});
     }catch(err){
-        console.error("Error saving user:",err);
-        res.status(500).json({error:"Error saving user"});
+        //console.error("Error saving user:",err);
+        res.status(400).json({err: errorHandler(err)});
     }
 };
