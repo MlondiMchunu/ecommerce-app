824638388be7ff939c1f25b0f62166c1261eaff7
Stopa <sikhuliso@gmail.com>
2024-05-23 19:02:29 +0200

    commit signin

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
-const User = require('../models/user')
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');// to generate signed token
+const expressJwt = require('express-jwt')//for authorization check
 const {errorHandler} = require('../helpers/dbErrorHandler')
 
 
@@ -8,8 +10,6 @@ exports.signup = async(req,res)=>{
     const user = new User(req.body);
 
     try{
-        //user.salt = undefined;
-        //user.hashed_password = undefined;
 
         const savedUser = await user.save();
 
@@ -21,4 +21,30 @@ exports.signup = async(req,res)=>{
         //console.error("Error saving user:",err);
         res.status(400).json({err: errorHandler(err)});
     }
+};
+
+exports.signin = (req,res)=>{
+    //find the user based on email
+    const {email,password} = req.body;
+    User.findOne({email},(err,user)=>{
+        if(err || !user){
+            return res.status(400).json({
+                err: 'User with that email does not exist. Please sign up'
+            });
+        }
+        //if user is found make sure the email and password match
+        //create authenticate method in user model
+        if(!user.authenticate(password)){
+            return res.status(401).json({
+                error: 'Email and password dont match'
+            })
+        }
+        //generate a signed token with user id and secret
+        const token = jwt.sign({__id: user._id},process.env.JWT_SECRET)
+        //persist the token  as 't' in cookie with expiry date
+        res.cookie('t',token,{expire: new Date() + 9999})
+        //return response with user and token frontend client
+        const {_id, name, email, role} = user
+        return res.json({token,user: {_id,email,name, role}})
+    });
 };
