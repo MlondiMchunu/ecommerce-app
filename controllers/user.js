5a753f88a1cbd69aa04ce607f8e33ba7d5d3ff07
Stopa <sikhuliso@gmail.com>
2024-05-23 20:39:29 +0200

    auth fix

--- controllers/user.js
+++ controllers/user.js
@@ -23,6 +23,8 @@ exports.signup = async(req,res)=>{
     }
 };
 
+/* findOne method no longer accepts a callback
+
 exports.signin = (req,res)=>{
     //find the user based on email
     const {email,password} = req.body;
@@ -47,4 +49,58 @@ exports.signin = (req,res)=>{
         const {_id, name, email, role} = user
         return res.json({token,user: {_id,email,name, role}})
     });
+};
+**hence check the method below
+*/
+
+exports.signin = async(req, res)=>{
+    const {email,password} = req.body;
+
+    try{
+
+        //Log request data
+        console.log("Signin request received:",req.body);
+
+
+        //check for missing email or password
+        if(!email || !password){
+            return res.status(400).json({
+                error: "Email and password are required."
+            });
+        }
+
+        //find user by email
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({
+                error: 'User with that email does not exist. Please sign up'
+            });
+        }
+        //authenticate user
+        if (!user.authenticate(password)){
+            return res.status(401).json({
+                error: 'Email and password do not match!'
+            });
+        }
+
+        //generate JWT token
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
+        if(!token){
+            throw new Error('Token generation failed');
+        }
+
+        //Set cookie
+        res.cookie('t',token,{expire:new Date() + 9999});
+
+        //REturn user and token
+        const {_id,name,email:userEmail,role} = user;
+        return res.json({token,user:{_id,email:userEmail,name,role}});
+    }
+    catch(err){
+        //Log the error details
+        console.error("Error during user authentication: ",err);
+        return res.status(500).json({
+            error: 'Internal server error during user authentication.'
+        });
+    }
 };
--- models/user.js
+++ models/user.js
@@ -41,16 +41,16 @@ const userSchema = new mongoose.Schema({
 userSchema.virtual('password')
 .set(function(password){
     this._password = password;
-    this.salt = uuidv1;
+    this.salt = uuidv1();
     this.hashed_password = this.encryptPassword(password);
 })
 .get(function(){
-    return this._password
-})
+    return this._password;
+});
 
 userSchema.methods = {
     authenticate: function(plainText){
-        return this.encryptPassword(plainText)===this.hashed_password;
+        return this.encryptPassword(plainText) === this.hashed_password;
         
     },
     encryptPassword: function(password){
@@ -58,7 +58,7 @@ userSchema.methods = {
         try{
             return crypto.createHmac("sha1",this.salt)
                             .update(password)
-                            .digest("hex")
+                            .digest("hex");
         } catch(err){
             return"";
         }
